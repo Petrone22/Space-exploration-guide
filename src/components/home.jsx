a2f6eec9105a0650feaf1d32731ea71c5618b557
Petrone22 <the1rashid@gmai.com>
2023-03-01 06:10:23 +0200

    added view for tablet on home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <div className="space-question-div">
+        <h4 className="space-question">SO, YOU WANT TO TRAVEL TO</h4>
+        <h1 className="space-word">SPACE</h1>
+        <p className="space-paragraph">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div className="explore-button-div">
+        <div className="explore">EXPLORE</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
