e9f5486686885f51083872a3114356213626edbb
Petrone22 <the1rashid@gmai.com>
2023-03-01 20:53:17 +0200

    finished mobile view for homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import navImg from "../assets/icon.svg";
+import hamburger from "../assets/hamburgerMenu.svg";
+import xicon from "../assets/xicon.svg";
 
 const Navbar = () => {
+  const [navOpened, setnavOpenend] = useState(false);
   return (
     <div className="nav-bar">
       <div className="nav-img">
@@ -23,6 +26,33 @@ const Navbar = () => {
             <div className="li-num">03</div> TECHNOLOGY
           </li>
         </ul>
+      </div>
+      <div
+        className="mobile-nav-menu"
+        style={{ right: navOpened ? "0px" : "-350px" }}
+      >
+        <ul className="mobile-nav-list">
+          <li>
+            <div className="mb-li-num">00</div> HOME
+          </li>
+          <li>
+            <div className="mb-li-num">01</div> DESTINATION
+          </li>
+          <li>
+            <div className="mb-li-num">02</div> CREW
+          </li>
+          <li>
+            <div className="mb-li-num">03</div> TECHNOLOGY
+          </li>
+        </ul>
+      </div>
+      <div
+        className="hamburger"
+        onClick={() => {
+          setnavOpenend((prevNav) => !prevNav);
+        }}
+      >
+        <img src={navOpened ? xicon : hamburger} alt="menu" />
       </div>
     </div>
   );
